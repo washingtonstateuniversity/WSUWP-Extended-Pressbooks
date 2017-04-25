7b1470a5f9b20f0442d2a92ea0e5a76663f2ccfc
Jeremy Felt <jeremy.felt@gmail.com>
2017-04-25 16:24:04 -0700

    Allow users to modify their admin color scheme
    This removes a handful of default actions added by Pressbooks

--- plugin.php
+++ plugin.php
@@ -7,3 +7,14 @@ Author: washingtonstateuniversity, jeremyfelt
 Version: 0.0.1
 Author URI: https://web.wsu.edu
 */
+
+namespace WSU\Pressbooks;
+
+add_action( 'plugins_loaded', '\WSU\Pressbooks\remove_pressbooks_hooks' );
+
+function remove_pressbooks_hooks() {
+	// Remove forced color scheme in admin.
+	remove_action( 'wp_login', '\Pressbooks\Activation::forcePbColors', 10 );
+	remove_action( 'profile_update', '\Pressbooks\Activation::forcePbColors' );
+	remove_action( 'user_register', '\Pressbooks\Activation::forcePbColors' );
+}
