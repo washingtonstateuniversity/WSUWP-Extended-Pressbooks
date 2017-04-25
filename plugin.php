ddc439be3f1850912bd5a339b817e469a94f3880
Jeremy Felt <jeremy.felt@gmail.com>
2017-04-25 16:22:29 -0700

    Add initial plugin files

---
+++ plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WSUWP Extended Pressbooks
+Plugin URI: https://github.com/washingtonstateuniversity/WSUWP-Extended-Pressbooks
+Description: WSU specific extensions to the Pressbooks plugin.
+Author: washingtonstateuniversity, jeremyfelt
+Version: 0.0.1
+Author URI: https://web.wsu.edu
+*/
