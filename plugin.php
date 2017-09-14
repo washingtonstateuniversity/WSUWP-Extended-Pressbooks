0cf09f83ac9b1409b821ebc16353f2ba16bf682f
Jeremy Felt <jeremy.felt@gmail.com>
2017-09-14 10:15:07 -0700

    Re-translate custom strings to include site URL hyphen support

--- plugin.php
+++ plugin.php
@@ -12,6 +12,7 @@ namespace WSU\Pressbooks;
 
 add_action( 'plugins_loaded', '\WSU\Pressbooks\remove_pressbooks_hooks' );
 add_filter( 'wpmu_validate_blog_signup', '\WSU\Pressbooks\allow_hyphens_in_site_url' );
+add_action( 'before_signup_form', 'WSU\Pressbooks\add_signup_text_filter' );
 
 function remove_pressbooks_hooks() {
 	// Remove forced color scheme in admin.
@@ -70,4 +71,37 @@ function allow_hyphens_in_site_url( $result ) {
 	$result['errors'] = $errors;
 
 	return $result;
+}
+
+/**
+ * Add a gettext filter on the signup page to adjust signup language.
+ *
+ * @since 0.0.2
+ */
+function add_signup_text_filter() {
+	add_filter( 'gettext', 'WSU\Pressbooks\custom_signup_text', 15, 2 );
+}
+
+/**
+ * Re-"translate" a custom Pressbooks string to let users know that hyphens
+ * can be used in book titles.
+ *
+ * @since 0.0.2
+ *
+ * @param string $translated_text
+ * @param string $untranslated_text
+ *
+ * @return string
+ */
+function custom_signup_text( $translated_text, $untranslated_text ) {
+	switch ( $untranslated_text ) {
+		case 'Must be at least 4 characters, letters and numbers only. It cannot be changed, so choose carefully!' :
+			$translated_text = __( 'Your webbook address is the web address where you will access and create your book. It must be at least 4 characters, letters, numbers, and hyphens only. It <strong>cannot be changed</strong>, so choose carefully! We suggest using the title of your book with no spaces.', 'pressbooks' );
+			break;
+		case 'If you&#8217;re not going to use a great site domain, leave it for a new user. Now have at it!' :
+			$translated_text = __( 'Your webbook address is the web address where you will access and create your book. It must be at least 4 characters, letters, numbers, and hyphens only. It <strong>cannot be changed</strong>, so choose carefully! We suggest using the title of your book with no spaces.', 'pressbooks' );
+			break;
+	}
+
+	return $translated_text;
 }
