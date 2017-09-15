3a99bc8971dfa387b120b57ed69ea103e3c05653
Jeremy Felt <jeremy.felt@gmail.com>
2017-09-15 16:31:09 -0700

    Bump version 0.0.4

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@ Plugin Name: WSUWP Extended Pressbooks
 Plugin URI: https://github.com/washingtonstateuniversity/WSUWP-Extended-Pressbooks
 Description: WSU specific extensions to the Pressbooks plugin.
 Author: washingtonstateuniversity, jeremyfelt
-Version: 0.0.3
+Version: 0.0.4
 Author URI: https://web.wsu.edu
 */
 
